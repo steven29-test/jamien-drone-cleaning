f70e1d55e88cfe58efa67b63bd95605f92c60eaf
Builder <builder@example.com>
2026-03-07 22:35:50 +1100

    Force version update to clear cache and deploy new reviews
    Added version tracking to force cache invalidation
    
    Assisted-By: Gordon

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import App from './App'
 import './index.css'
+import { APP_VERSION } from './version'
+
+console.log('App Version:', APP_VERSION)
 
 const theme = createTheme({
   typography: {
